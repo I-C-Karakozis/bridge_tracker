a20895910bce57ffab2df8e3eee9b41dc3eca248
Yannis Karakozis <I-C-Karakozis@users.noreply.github.com>
2018-02-25 19:51:37 -0500

    directory refactor

--- base_classifier.py
+++ base_classifier.py
@@ -3,76 +3,7 @@ import cv2
 import numpy as np
 import os
 
-import Cards
-import stats
-
-### ---- INITIALIZATION ---- ###
-
-# confidence level for background/foreground predictions
-CONFIDENCE = 99
-
-# we use 3 degrees of freedom bc we use the RGB color space
-DOF = 3
-
-### ------------------------ ###
-
-def model_boundary(img):
-    ''' 
-    Model the color distribution of the boundary pixels as 
-    a Multivariate Gaussian with diagonal covariance matrix
-    '''
-
-    # compute boundary size from image dimensions
-    dims = np.shape(img)
-    boundary_size = 2*dims[0] + 2 * dims[1]
-    boundary = np.zeros((boundary_size, 3))
-    count = 0
-
-    # collect pixels of leftmost and rightmost image boundaries
-    for col in [0, dims[1]-1]:
-        for row in range(dims[0]):
-            boundary[count] = img[row][col]
-            count = count + 1
-
-    # collect pixels of top and bottom image boundaries
-    for row in [0, dims[0]-1]:
-        for col in range(dims[1]):
-            boundary[count] = img[row][col]
-            count = count + 1
-
-    # compute multivariate gaussian 
-    mu = np.mean(boundary, axis=0)
-    sigmas = np.std(boundary, axis=0)
-    return mu, sigmas
-
-def remove_background(img, mu, sigmas):
-    ''' 
-    Remove form img background pixels whose color distirbution is modeled
-    by Multivariate Gaussian with mean mu and covariance matrix diag(sigmas^2).
-    '''
-    dims = np.shape(img)
-
-    zscore = np.square((img - mu).astype(np.float) / sigmas)
-    zscore = np.reshape(np.apply_over_axes(np.sum, zscore, [2]), (dims[0], dims[1]))
-    zscore = np.repeat(zscore[:, :, np.newaxis], 3, axis=2)
-    mask = np.where(stats.h_test(zscore, confidence=CONFIDENCE, 
-                                       dof=DOF), 0, 255).astype(np.uint8)
-
-    black_frame = np.zeros(dims, dtype=np.uint8)
-    foreground = np.where(mask > 0, img, black_frame)
-    return foreground
-
-def correct_img_dim(image):
-    '''Adjust/Rotate image into 720x1280 (height by width). '''
-
-    # rotate image so that height < width
-    dims = np.shape(image)
-    if dims[0] > dims[1]:
-        image = np.transpose(image)
-
-    # resize image into (IM_HEIGHT x IM_WIDTH)
-    corrected = cv2.resize(image, (IM_WIDTH, IM_HEIGHT))
-    return corrected
+from tools import Cards, imeditor
 
 def main(args):
 
@@ -83,8 +14,8 @@ def main(args):
 
     # remove background felt from image
     image = cv2.imread(args.test_img)
-    green_mu, green_sigmas = model_boundary(image)
-    no_felt = remove_background(image, green_mu, green_sigmas)
+    green_mu, green_sigmas = imeditor.model_boundary(image)
+    no_felt = imeditor.remove_background(image, green_mu, green_sigmas)
 
     # Pre-process camera image (gray, blur, and threshold it)
     pre_proc = Cards.preprocess_image(no_felt)
