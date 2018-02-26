1e6f997f0a5bf420da89114607b1b62c324dc514
Yannis Karakozis <I-C-Karakozis@users.noreply.github.com>
2018-02-25 19:42:30 -0500

    first correct detection (ace of clubs)

--- base_classifier.py
+++ base_classifier.py
@@ -1,7 +1,9 @@
 import argparse
 import cv2
 import numpy as np
+import os
 
+import Cards
 import stats
 
 ### ---- INITIALIZATION ---- ###
@@ -60,16 +62,73 @@ def remove_background(img, mu, sigmas):
     foreground = np.where(mask > 0, img, black_frame)
     return foreground
 
+def correct_img_dim(image):
+    '''Adjust/Rotate image into 720x1280 (height by width). '''
+
+    # rotate image so that height < width
+    dims = np.shape(image)
+    if dims[0] > dims[1]:
+        image = np.transpose(image)
+
+    # resize image into (IM_HEIGHT x IM_WIDTH)
+    corrected = cv2.resize(image, (IM_WIDTH, IM_HEIGHT))
+    return corrected
+
 def main(args):
 
-    img = cv2.imread(args.test_img)
-    green_mu, green_sigmas = model_boundary(img)
-    no_felt = remove_background(img, green_mu, green_sigmas)
+    # Load the train rank and suit images
+    path = os.path.dirname(os.path.abspath(__file__))
+    train_ranks = Cards.load_ranks( path + '/Card_Imgs/')
+    train_suits = Cards.load_suits( path + '/Card_Imgs/')
 
-    cv2.imshow("no_felt", no_felt)
+    # remove background felt from image
+    image = cv2.imread(args.test_img)
+    green_mu, green_sigmas = model_boundary(image)
+    no_felt = remove_background(image, green_mu, green_sigmas)
+
+    # Pre-process camera image (gray, blur, and threshold it)
+    pre_proc = Cards.preprocess_image(no_felt)
+    
+    # Find and sort the contours of all cards in the image (query cards)
+    cnts_sort, cnt_is_card = Cards.find_cards(pre_proc)
+
+    # If there are no contours, do nothing
+    if len(cnts_sort) != 0:
+
+        # Initialize a new "cards" list to assign the card objects.
+        # k indexes the newly made array of cards.
+        cards = []
+        k = 0
+
+        # For each contour detected:
+        for i in range(len(cnts_sort)):
+            if (cnt_is_card[i] == 1):
+                # Create a card object from the contour and append it to the list of cards.
+                # preprocess_card function takes the card contour and contour and
+                # determines the cards properties (corner points, etc). It generates a
+                # flattened 200x300 image of the card, and isolates the card's
+                # suit and rank from the image.
+                cards.append(Cards.preprocess_card(cnts_sort[i],image))
+
+                # Find the best rank and suit match for the card.
+                cards[k].best_rank_match,cards[k].best_suit_match,cards[k].rank_diff,cards[k].suit_diff = Cards.match_card(cards[k],train_ranks,train_suits)
+
+                # Draw center point and match result on the image.
+                image = Cards.draw_results(image, cards[k])
+                k = k + 1
+        
+        # Draw card contours on image (have to do contours all at once or
+        # they do not show up properly for some reason)
+        if (len(cards) != 0):
+            temp_cnts = []
+            for i in range(len(cards)):
+                temp_cnts.append(cards[i].contour)
+            cv2.drawContours(image,temp_cnts, -1, (255,0,0), 2)
+        
+    cv2.imshow("Card Detector",image)
     key = cv2.waitKey(100000) & 0xFF
     if key == ord("q"):
-        return
+        pass
 
     return
 
